d30a59b99ce58beb64eccef2d6be4ab398d3afaf
gursharan1512 <sharanghotra1512@gmail.com>
2018-10-22 12:42:55 +0530

    updating script files

--- dockerFiles/dockerContainers.sh
+++ dockerFiles/dockerContainers.sh
@@ -2,5 +2,6 @@
 ## Incase of any error, setting option to exit from shell script
 	 set -e 
 ##
-docker-compose up -d
-docker exec -it sharanghotra15121512_sparkApp_1 bash
+userId='sharanghotra15121512'
+jarFile='dockerRedisMysql2WithSaveToTextFile.jar'
+docker exec -it $userId'_sparkApp_1' spark-submit --class test 'sparkJob/'$jarFile
